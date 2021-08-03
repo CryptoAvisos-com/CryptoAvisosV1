46f9f85b0718d13486febb43fbbb3bb870b1a10a
TheAustrian1998 <theaustrianeth@gmail.com>
2021-08-03 11:23:57 -0300

    Added test: claim fees

--- tests/testCryptoAvisosV1.js
+++ tests/testCryptoAvisosV1.js
@@ -73,11 +73,64 @@ describe("CryptoAvisosV1", function () {
         console.log('ETH balance ->', ethers.utils.formatUnits(ethAcc2), ethers.utils.formatUnits(ethSeller), ethers.utils.formatUnits(ethContract));
     });
 
-    // it("Should claim fees in ETH, succesfully...", async function () {
+    it("Should claim fees in DAI, succesfully...", async function () {
+        //Deploy Example DAI
+        const DAI = await ethers.getContractFactory("DAI");
+        const dai = await DAI.deploy(ethers.utils.parseUnits('10000000',"ether"));
+        await dai.deployed();
 
-    // });
+        //Transfer DAI to account 2
+        await dai.transfer(this.accounts[2].address, ethers.utils.parseUnits('10000',"ether"));
 
-    // it("Should claim fees in DAI, succesfully...", async function () {
+        //Submit product
+        let daiAddress = dai.address;
+        const submitProductTx = await this.cryptoAvisosV1.submitProduct(256, this.accounts[1].address, ethers.utils.parseUnits('200',"ether"), daiAddress);
+        let receipt = await submitProductTx.wait();
+        expect(receipt.status).to.equal(1);
 
-    // });
+        //View product
+        let viewProduct = await this.cryptoAvisosV1.viewProduct(256);
+
+        //Approve DAI
+        const approveTx = await dai.connect(this.accounts[2]).approve(this.cryptoAvisosV1.address, ethers.utils.parseUnits('10000',"ether"));
+
+        //Pay product
+        const payProductTx = await this.cryptoAvisosV1.connect(this.accounts[2]).payProduct(256);
+        let receiptPay = await payProductTx.wait();
+        expect(receiptPay.status).to.equal(1);
+
+        //DAI amount
+        let daiBalanceAcc2 = await dai.balanceOf(this.accounts[2].address);
+        let daiBalanceSeller = await dai.balanceOf(this.accounts[1].address);
+        let daiBalanceContract = await dai.balanceOf(this.cryptoAvisosV1.address);
+        console.log('DAI balance ->', ethers.utils.formatUnits(daiBalanceAcc2), ethers.utils.formatUnits(daiBalanceSeller), ethers.utils.formatUnits(daiBalanceContract));
+
+        let claimTx = await this.cryptoAvisosV1.connect(this.accounts[0]).claimFee(daiAddress);
+        let daiBalanceOwner = await dai.balanceOf(this.accounts[0].address);
+        console.log('DAI balance ->', ethers.utils.formatUnits(daiBalanceOwner));
+    });
+
+    it("Should claim fees in ETH, succesfully...", async function () {
+        //Submit product
+        const submitProductTx = await this.cryptoAvisosV1.submitProduct(256, this.accounts[1].address, ethers.utils.parseUnits('200',"ether"), ethers.constants.AddressZero);
+        let receipt = await submitProductTx.wait();
+        expect(receipt.status).to.equal(1);
+
+        //View product
+        let viewProduct = await this.cryptoAvisosV1.viewProduct(256);
+
+        //Pay product
+        const payProductTx = await this.cryptoAvisosV1.connect(this.accounts[2]).payProduct(256, { "value": ethers.utils.parseUnits('200',"ether") });
+        let receiptPay = await payProductTx.wait();
+        expect(receiptPay.status).to.equal(1);
+
+        let ethAcc2 = await this.accounts[2].getBalance();
+        let ethSeller = await this.accounts[1].getBalance();
+        let ethContract = await this.cryptoAvisosV1.viewETHBalance();
+        console.log('ETH balance ->', ethers.utils.formatUnits(ethAcc2), ethers.utils.formatUnits(ethSeller), ethers.utils.formatUnits(ethContract));
+
+        let claimTx = await this.cryptoAvisosV1.connect(this.accounts[0]).claimFee(ethers.constants.AddressZero);
+        let ethBalanceOwner = await this.accounts[0].getBalance();
+        console.log('ETH balance owner ->', ethers.utils.formatUnits(ethBalanceOwner));
+    });
 });
