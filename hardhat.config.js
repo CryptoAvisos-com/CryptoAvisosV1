66bf609831deb8ff2b38a4485bb893fe4c072861
TheAustrian1998 <theaustrianeth@gmail.com>
2021-08-03 10:56:00 -0300

    Added rinkeby network

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,7 +3,14 @@
  */
 
 require("@nomiclabs/hardhat-waffle");
+const { mnemonic, url } = require('./secrets.json');
 
 module.exports = {
   solidity: "0.8.0",
+  networks: {
+    rinkeby: {
+      url: url,
+      accounts: { mnemonic }
+    }
+  }
 };
