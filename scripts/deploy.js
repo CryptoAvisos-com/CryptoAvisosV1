6435043f44227a8246a5b4673c45dcfc266c9e4b
TheAustrian1998 <theaustrianeth@gmail.com>
2021-10-18 20:28:19 -0300

    Modify deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,21 +1,54 @@
 const hre = require("hardhat");
+const {
+    CryptoAvisosEthereumMultiSig,
+    CryptoAvisosBSCMultiSig,
+    CryptoAvisosPolygonMultiSig
+} = require("../registry.json");
 
 async function main() {
     await hre.run("clean");
     await hre.run("compile");
+
     //Deploy CAV1
-    let initialFee = 2;
+    let initialFee = 1;
     const CryptoAvisosV1 = await hre.ethers.getContractFactory("CryptoAvisosV1");
-    const cryptoAvisosV1 = await CryptoAvisosV1.deploy(initialFee);
-    await cryptoAvisosV1.deployed();
+    const cryptoAvisosV1 = await CryptoAvisosV1.deploy(ethers.utils.parseUnits(String(initialFee)));
+
+    console.log("Waiting to confirm");
+    await cryptoAvisosV1.deployTransaction.wait(2);
 
     console.log("Deployed CryptoAvisosV1 to:", cryptoAvisosV1.address);
 
+    let multisig;
+    let network = await hre.ethers.provider.getNetwork();
+
+    //Select CA multisig to transfer ownership
+    switch (network.chainId) {
+        case 1:
+            multisig = CryptoAvisosEthereumMultiSig;
+            break;
+        case 56:
+            multisig = CryptoAvisosBSCMultiSig;
+            break;
+        case 137:
+            multisig = CryptoAvisosPolygonMultiSig;
+            break;
+        default:
+            multisig = "";
+            break;
+    }
+
+    //Transfer ownership
+    if (multisig || multisig != "") {
+        await cryptoAvisosV1.transferOwnership(multisig);
+        console.log("Owner transferred to:", multisig);
+    }
+
     //Verify
-    // await hre.run("verify:verify", {
-    //     address: cryptoAvisosV1.address,
-    //     constructorArguments: [initialFee],
-    // });
+    await hre.run("verify:verify", {
+        address: cryptoAvisosV1.address,
+        constructorArguments: [ethers.utils.parseUnits(String(initialFee))],
+    });
 
 }
 
