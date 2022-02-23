c38a22b5c7afd071514878c2a59d0730c9430b54
TheAustrian1998 <theaustrianeth@gmail.com>
2022-02-23 20:04:43 -0300

    Added Ticket mechanism

--- contracts/CryptoAvisosV1.sol
+++ contracts/CryptoAvisosV1.sol
@@ -6,58 +6,70 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract CryptoAvisosV1 is Ownable {
 
-    mapping(uint256 => Product) public productMapping;
-    mapping(address => uint256) public claimableFee;
-    uint256[] private productsIds;
-    uint256 public fee;
-    uint256 public lastUnlockTimeFee;
-    uint256 public lastFeeToSet;
+    mapping(uint => Product) public productMapping; //productId in CA platform => Product
+    mapping(uint => Ticket) public productTicketsMapping; //uint(keccak256(productId, buyer, blockNumber, product.stock)) => Ticket
+    mapping(address => uint) public claimableFee;
+    uint[] private productsIds;
+    uint[] private ticketsIds;
+    uint public fee;
+    uint public lastUnlockTimeFee;
+    uint public lastFeeToSet;
 
-    event ProductSubmitted(uint256 productId);
-    event ProductPaid(uint256 productId);
-    event ProductReleased(uint256 productId);
-    event ProductUpdated(uint256 productId);
-    event ProductRefunded(uint256 productId);
-    event ProductMarkAsPaid(uint256 productId);
-    event FeeSetted(uint256 previousFee, uint256 newFee);
-    event FeesClaimed(address receiver, address token, uint256 quantity);
+    event ProductSubmitted(uint productId);
+    event ProductPaid(uint productId, uint ticketId);
+    event PayReleased(uint productId, uint tickerId);
+    event ProductUpdated(uint productId);
+    event ProductRefunded(uint productId, uint ticketId);
+    event SwitchChanged(uint productId, bool isEnabled);
+    event FeeSetted(uint previousFee, uint newFee);
+    event FeesClaimed(address receiver, address token, uint quantity);
     event PreparedFee(uint fee, uint unlockTime);
     event StockAdded(uint productId, uint stockAdded);
     event StockRemoved(uint productId, uint stockRemoved);
 
-    constructor(uint256 newFee){
+    constructor(uint newFee){
         _setFee(newFee);
     }
 
     struct Product {
-        uint256 price; //In WEI
-        Status status; 
+        uint price; //In WEI
         address payable seller;
+        address token; //Contract address or 0x00 if it"s native coin
+        uint stock;
+        bool enabled;
+    }
+
+    struct Ticket {
+        uint productId;
+        Status status;
         address payable buyer;
-        address token; //Contract address or 0x00 if it's native coin
-        uint256 feeCharged; //Holds charged fee, in case admin need to refund and fee has change between pay and refund time
-        uint256 stock;
+        address tokenPaid; //Holds contract address or 0x00 if it"s native coin used in payment
+        uint feeCharged; //Holds charged fee, in case admin need to refund and fee has change between pay and refund time
+        uint pricePaid; //Holds price paid at momento of payment (without fee)
     }
 
     enum Status {
-        FORSELL,
         WAITING,
         SOLD
     }
 
-    function getProductsIds() external view returns (uint256[] memory) {
+    function getProductsIds() external view returns (uint[] memory) {
         return productsIds;
     }
 
-    function _setFee(uint256 newFee) internal {
+    function getTicketsIds() external view returns (uint[] memory) {
+        return ticketsIds;
+    }
+
+    function _setFee(uint newFee) internal {
         //Set fee. Example: 10e18 = 10%
-        require(newFee <= 100e18, '!fee');
-        uint256 previousFee = fee;
+        require(newFee <= 100e18, "!fee");
+        uint previousFee = fee;
         fee = newFee;
         emit FeeSetted(previousFee, newFee);
     }
 
-    function prepareFee(uint256 newFee) external onlyOwner {
+    function prepareFee(uint newFee) external onlyOwner {
         //Prepare to set fee (wait 7 days to set. Timelock kind of)
         lastUnlockTimeFee = block.timestamp + 7 days;
         lastFeeToSet = newFee;
@@ -72,7 +84,7 @@ contract CryptoAvisosV1 is Ownable {
         lastUnlockTimeFee = 0;
     }
 
-    function claimFees(address token, uint256 quantity) external payable onlyOwner {
+    function claimFees(address token, uint quantity) external payable onlyOwner {
         //Claim fees originated of paying a product
         require(claimableFee[token] >= quantity, "!funds");
         claimableFee[token] -= quantity;
@@ -86,107 +98,111 @@ contract CryptoAvisosV1 is Ownable {
         emit FeesClaimed(msg.sender, token, quantity);
     }
 
-    function submitProduct(uint256 productId, address payable seller, uint256 price, address token, uint256 stock) external onlyOwner {
+    function submitProduct(uint productId, address payable seller, uint price, address token, uint stock) external onlyOwner {
         //Submit a product
         require(productId != 0, "!productId");
         require(price != 0, "!price");
         require(seller != address(0), "!seller");
+        require(stock != 0, "!stock");
         require(productMapping[productId].seller == address(0), "alreadyExist");
-        Product memory product = Product(price, Status.FORSELL, seller, payable(address(0)), token, 0, stock);
+        Product memory product = Product(price, seller, token, stock, true);
         productMapping[productId] = product;
         productsIds.push(productId);
         emit ProductSubmitted(productId);
     }
 
-    function markAsPaid(uint256 productId) external onlyOwner {
-        //This function mark as paid a product when is paid in other chain
+    function switchEnable(uint productId, bool isEnabled) external onlyOwner {
+        //This function enable or disable a product
         Product memory product = productMapping[productId];
         require(product.seller != address(0), "!exist");
-        require(Status.SOLD != product.status, 'sold');
-        product.status = Status.SOLD;
+        product.enabled = isEnabled;
         productMapping[productId] = product;
-        emit ProductMarkAsPaid(productId);
+        emit SwitchChanged(productId, isEnabled);
     }
 
-    function payProduct(uint256 productId) external payable {
+    function payProduct(uint productId) external payable {
         //Pay a specific product
         Product memory product = productMapping[productId];
         require(product.seller != address(0), "!exist");
-        require(Status.FORSELL == product.status, 'sold');
+        require(product.enabled, "!enabled");
         require(product.stock != 0, "!stock");
 
         if (product.token == address(0)) {
             //Pay with ether (or native coin)
-            require(msg.value == product.price, '!msg.value');
+            require(msg.value == product.price, "!msg.value");
         }else{
             //Pay with token
             IERC20(product.token).transferFrom(msg.sender, address(this), product.price);
         }
 
-        uint256 toFee = product.price * fee / 100e18;
+        uint toFee = product.price * fee / 100e18;
         claimableFee[product.token] += toFee;
 
-        product.feeCharged = toFee;
-        product.status = Status.WAITING;
-        product.buyer = payable(msg.sender);
+        //Create ticket
+        uint ticketId = uint(keccak256(abi.encode(productId, msg.sender, block.number, product.stock)));
+        productTicketsMapping[ticketId] = Ticket(productId, Status.WAITING, payable(msg.sender), product.token, toFee, product.price);
+        ticketsIds.push(ticketId);
+
         product.stock -= 1;
         productMapping[productId] = product;
-        emit ProductPaid(productId);
+        emit ProductPaid(productId, ticketId);
     }
 
-    function releasePay(uint256 productId) external onlyOwner {
+    function releasePay(uint ticketId) external onlyOwner {
         //Release pay to seller
-        Product memory product = productMapping[productId];
-        require(product.seller != address(0), "!exist");
-        require(Status.WAITING == product.status, '!waiting');
-        uint256 finalPrice = product.price - product.feeCharged;
+        Ticket memory ticket = productTicketsMapping[ticketId];
 
-        if (product.token == address(0)) {
+        Product memory product = productMapping[ticket.productId];
+        require(Status.WAITING == ticket.status, "!waiting");
+        uint finalPrice = ticket.pricePaid;
+
+        if (ticket.tokenPaid == address(0)) {
             //Pay with ether (or native coin)
             product.seller.transfer(finalPrice);
         }else{
             //Pay with token
-            IERC20(product.token).transfer(product.seller, finalPrice);
+            IERC20(ticket.tokenPaid).transfer(product.seller, finalPrice);
         }
 
-        product.status = Status.SOLD;
-        productMapping[productId] = product;
-        emit ProductReleased(productId);
+        ticket.status = Status.SOLD;
+        productTicketsMapping[ticketId] = ticket;
+        emit PayReleased(ticket.productId, ticketId);
     }
 
-    function updateProduct(uint256 productId, address payable seller, uint256 price, address token, uint256 stock) external onlyOwner {
+    function updateProduct(uint productId, address payable seller, uint price, address token, uint stock) external onlyOwner {
         //Update a product
         require(productId != 0, "!productId");
         require(price != 0, "!price");
         require(seller != address(0), "!seller");
         Product memory product = productMapping[productId];
-        require(product.status == Status.FORSELL || product.status == Status.WAITING, "!forSell");
         require(product.seller != address(0), "!exist");
-        product = Product(price, Status.FORSELL, seller, payable(address(0)), token, 0, stock);
+        product = Product(price, seller, token, stock, true);
         productMapping[productId] = product;
         emit ProductUpdated(productId);
     }
 
-    function refundProduct(uint256 productId) external onlyOwner {
+    function refundProduct(uint ticketId) external onlyOwner {
         //Return funds to buyer
-        require(productId != 0, "!productId");
-        Product memory product = productMapping[productId];
+        Ticket memory ticket = productTicketsMapping[ticketId];
+
+        require(ticket.productId != 0, "!productId");
+        Product memory product = productMapping[ticket.productId];
         require(product.seller != address(0), "!exist");
-        require(product.status == Status.WAITING, "!waiting");
-        if(product.token == address(0)){
+        if(ticket.tokenPaid == address(0)){
             //ETH
-            payable(product.buyer).transfer(product.price);
+            ticket.buyer.transfer(ticket.pricePaid);
         }else{
             //ERC20
-            IERC20(product.token).transfer(product.buyer, product.price);
+            IERC20(ticket.tokenPaid).transfer(ticket.buyer, ticket.pricePaid);
         }
-        claimableFee[product.token] -= product.feeCharged;
-        product.status = Status.SOLD;
-        productMapping[productId] = product;
-        emit ProductRefunded(productId);
+        claimableFee[ticket.tokenPaid] -= ticket.feeCharged;
+        ticket.status = Status.SOLD;
+        
+        productTicketsMapping[ticketId] = ticket;
+        emit ProductRefunded(ticket.productId, ticketId);
     }
 
-    function addStock(uint256 productId, uint256 stockToAdd) external onlyOwner {
+    function addStock(uint productId, uint stockToAdd) external onlyOwner {
         //Add stock to a product
         Product memory product = productMapping[productId];
         require(productId != 0, "!productId");
@@ -197,7 +213,7 @@ contract CryptoAvisosV1 is Ownable {
         emit StockAdded(productId, stockToAdd);
     }
 
-    function removeStock(uint256 productId, uint256 stockToRemove) external onlyOwner {
+    function removeStock(uint productId, uint stockToRemove) external onlyOwner {
         //Add stock to a product
         Product memory product = productMapping[productId];
         require(productId != 0, "!productId");
