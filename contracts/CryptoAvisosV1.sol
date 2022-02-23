7b5d2c72ce65e81fb32f66d6b6e0d0d30e828a8d
TheAustrian1998 <theaustrianeth@gmail.com>
2022-02-23 11:06:01 -0300

    Add stock in product, fix error msg

--- contracts/CryptoAvisosV1.sol
+++ contracts/CryptoAvisosV1.sol
@@ -1,4 +1,4 @@
-//SPDX-License-Identifier: Unlicense
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
@@ -22,6 +22,8 @@ contract CryptoAvisosV1 is Ownable {
     event FeeSetted(uint256 previousFee, uint256 newFee);
     event FeesClaimed(address receiver, address token, uint256 quantity);
     event PreparedFee(uint fee, uint unlockTime);
+    event StockAdded(uint productId, uint stockAdded);
+    event StockRemoved(uint productId, uint stockRemoved);
 
     constructor(uint256 newFee){
         _setFee(newFee);
@@ -34,6 +36,7 @@ contract CryptoAvisosV1 is Ownable {
         address payable buyer;
         address token; //Contract address or 0x00 if it's native coin
         uint256 feeCharged; //Holds charged fee, in case admin need to refund and fee has change between pay and refund time
+        uint256 stock;
     }
 
     enum Status {
@@ -48,7 +51,7 @@ contract CryptoAvisosV1 is Ownable {
 
     function _setFee(uint256 newFee) internal {
         //Set fee. Example: 10e18 = 10%
-        require(newFee <= 100e18, 'Fee bigger than 100%');
+        require(newFee <= 100e18, '!fee');
         uint256 previousFee = fee;
         fee = newFee;
         emit FeeSetted(previousFee, newFee);
@@ -63,15 +66,15 @@ contract CryptoAvisosV1 is Ownable {
 
     function implementFee() external onlyOwner {
         //Set fee after 7 days
-        require(lastUnlockTimeFee > 0, "not prepared");
-        require(lastUnlockTimeFee <= block.timestamp, "not unlocked yet");
+        require(lastUnlockTimeFee > 0, "!prepared");
+        require(lastUnlockTimeFee <= block.timestamp, "!unlocked");
         _setFee(lastFeeToSet);
         lastUnlockTimeFee = 0;
     }
 
     function claimFees(address token, uint256 quantity) external payable onlyOwner {
         //Claim fees originated of paying a product
-        require(claimableFee[token] >= quantity, "not enough funds");
+        require(claimableFee[token] >= quantity, "!funds");
         claimableFee[token] -= quantity;
         if(token == address(0)){
             //ETH
@@ -83,13 +86,13 @@ contract CryptoAvisosV1 is Ownable {
         emit FeesClaimed(msg.sender, token, quantity);
     }
 
-    function submitProduct(uint256 productId, address payable seller, uint256 price, address token) external onlyOwner {
+    function submitProduct(uint256 productId, address payable seller, uint256 price, address token, uint256 stock) external onlyOwner {
         //Submit a product
-        require(productId != 0, "productId cannot be zero");
-        require(price != 0, "price cannot be zero");
-        require(seller != address(0), "seller cannot be zero address");
-        require(productMapping[productId].seller == address(0), "productId already exist");
-        Product memory product = Product(price, Status.FORSELL, seller, payable(address(0)), token, 0);
+        require(productId != 0, "!productId");
+        require(price != 0, "!price");
+        require(seller != address(0), "!seller");
+        require(productMapping[productId].seller == address(0), "alreadyExist");
+        Product memory product = Product(price, Status.FORSELL, seller, payable(address(0)), token, 0, stock);
         productMapping[productId] = product;
         productsIds.push(productId);
         emit ProductSubmitted(productId);
@@ -98,8 +101,8 @@ contract CryptoAvisosV1 is Ownable {
     function markAsPaid(uint256 productId) external onlyOwner {
         //This function mark as paid a product when is paid in other chain
         Product memory product = productMapping[productId];
-        require(product.seller != address(0), "cannot mark as paid a non existing product");
-        require(Status.SOLD != product.status, 'Product already sold');
+        require(product.seller != address(0), "!exist");
+        require(Status.SOLD != product.status, 'sold');
         product.status = Status.SOLD;
         productMapping[productId] = product;
         emit ProductMarkAsPaid(productId);
@@ -108,12 +111,13 @@ contract CryptoAvisosV1 is Ownable {
     function payProduct(uint256 productId) external payable {
         //Pay a specific product
         Product memory product = productMapping[productId];
-        require(product.seller != address(0), "cannot pay a non existing product");
-        require(Status.FORSELL == product.status, 'Product already sold');
+        require(product.seller != address(0), "!exist");
+        require(Status.FORSELL == product.status, 'sold');
+        require(product.stock != 0, "!stock");
 
         if (product.token == address(0)) {
             //Pay with ether (or native coin)
-            require(msg.value >= product.price, 'Not enough ETH sended');
+            require(msg.value == product.price, '!msg.value');
         }else{
             //Pay with token
             IERC20(product.token).transferFrom(msg.sender, address(this), product.price);
@@ -125,6 +129,7 @@ contract CryptoAvisosV1 is Ownable {
         product.feeCharged = toFee;
         product.status = Status.WAITING;
         product.buyer = payable(msg.sender);
+        product.stock -= 1;
         productMapping[productId] = product;
         emit ProductPaid(productId);
     }
@@ -132,8 +137,8 @@ contract CryptoAvisosV1 is Ownable {
     function releasePay(uint256 productId) external onlyOwner {
         //Release pay to seller
         Product memory product = productMapping[productId];
-        require(product.seller != address(0), "cannot release pay for a non existing product");
-        require(Status.WAITING == product.status, 'Not allowed to release pay');
+        require(product.seller != address(0), "!exist");
+        require(Status.WAITING == product.status, '!waiting');
         uint256 finalPrice = product.price - product.feeCharged;
 
         if (product.token == address(0)) {
@@ -149,25 +154,25 @@ contract CryptoAvisosV1 is Ownable {
         emit ProductReleased(productId);
     }
 
-    function updateProduct(uint256 productId, address payable seller, uint256 price, address token) external onlyOwner {
+    function updateProduct(uint256 productId, address payable seller, uint256 price, address token, uint256 stock) external onlyOwner {
         //Update a product
-        require(productId != 0, "productId cannot be zero");
-        require(price != 0, "price cannot be zero");
-        require(seller != address(0), "seller cannot be zero address");
+        require(productId != 0, "!productId");
+        require(price != 0, "!price");
+        require(seller != address(0), "!seller");
         Product memory product = productMapping[productId];
-        require(product.status == Status.FORSELL || product.status == Status.WAITING, "cannot update a sold product");
-        require(product.seller != address(0), "cannot update a non existing product");
-        product = Product(price, Status.FORSELL, seller, payable(address(0)), token, 0);
+        require(product.status == Status.FORSELL || product.status == Status.WAITING, "!forSell");
+        require(product.seller != address(0), "!exist");
+        product = Product(price, Status.FORSELL, seller, payable(address(0)), token, 0, stock);
         productMapping[productId] = product;
         emit ProductUpdated(productId);
     }
 
     function refundProduct(uint256 productId) external onlyOwner {
         //Return funds to buyer
-        require(productId != 0, "productId cannot be zero");
+        require(productId != 0, "!productId");
         Product memory product = productMapping[productId];
-        require(product.seller != address(0), "cannot refund a non existing product");
-        require(product.status == Status.WAITING, "cannot refund a non waiting product");
+        require(product.seller != address(0), "!exist");
+        require(product.status == Status.WAITING, "!waiting");
         if(product.token == address(0)){
             //ETH
             payable(product.buyer).transfer(product.price);
@@ -179,6 +184,28 @@ contract CryptoAvisosV1 is Ownable {
         product.status = Status.SOLD;
         productMapping[productId] = product;
         emit ProductRefunded(productId);
+    }
+
+    function addStock(uint256 productId, uint256 stockToAdd) external onlyOwner {
+        //Add stock to a product
+        Product memory product = productMapping[productId];
+        require(productId != 0, "!productId");
+        require(stockToAdd != 0, "!stockToAdd");
+        require(product.seller != address(0), "!exist");
+        product.stock += stockToAdd;
+        productMapping[productId] = product;
+        emit StockAdded(productId, stockToAdd);
+    }
+
+    function removeStock(uint256 productId, uint256 stockToRemove) external onlyOwner {
+        //Add stock to a product
+        Product memory product = productMapping[productId];
+        require(productId != 0, "!productId");
+        require(product.stock >= stockToRemove, "!stockToRemove");
+        require(product.seller != address(0), "!exist");
+        product.stock -= stockToRemove;
+        productMapping[productId] = product;
+        emit StockRemoved(productId, stockToRemove);
     }
     
 }
